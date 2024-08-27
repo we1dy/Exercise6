e91894ddf4c7bdfd6918a5f87a18d8b2618feb59
we1dy <ladyarboleda26@gmail.com>
2024-08-28 01:11:17 +0800

    Add project files.

---
+++ Exercise6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Exercise
+{
+	public static void Main()
+	{
+		float PerHeight;
+		Console.Write("\n\n");
+		Console.Write("Accept the height of a person in centimeter");
+
+		Console.Write("Input the height of the person (in centimetres):");
+		PerHeight = Convert.ToInt32(Console.ReadLine());
+
+		if (PerHeight < 150.0)
+			Console.Write("The person is Dwarf. \n\n");
+		else if ((PerHeight >= 150.0) && (PerHeight <= 165.0))
+			Console.Write("The person is  average heighted. \n\n");
+		else if ((PerHeight >= 165.0) && (PerHeight <= 195.0))
+			Console.Write("The person is taller. \n\n");
+		else
+			Console.Write("Abnormal height.\n\n");
+	}
+
+}
+
